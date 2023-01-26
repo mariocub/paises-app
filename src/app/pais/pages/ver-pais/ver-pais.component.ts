a1f230a406babdf8b42666c1381ebf8345358e97
Mario Cubells <mariocubellsgarrido@gmail.com>
2023-01-26 13:07:02 +0100

    final commit

---
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs';
+import { Country } from '../../interfaces/pais-interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-ver-pais',
+  templateUrl: './ver-pais.component.html',
+})
+export class VerPaisComponent implements OnInit {
+  pais!: Country;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisService
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.paisService.getPaisPorAlpha(id)),
+        tap(console.log)
+      )
+      .subscribe((pais) => {
+        console.log(pais);
+        this.pais = pais;
+      });
+  }
+}
